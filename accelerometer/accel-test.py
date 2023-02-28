fba907512cee19a92c2b2876adc67376b7e3d458
HineSite <kevin@hine.site>
2023-02-28 15:21:49 -0500

    Moved some things around because the order of z, y, x makes more sense to me as x, y, z. Which is probably why a couple variables were out of order.

--- accelerometer/accel-test.py
+++ accelerometer/accel-test.py
@@ -17,75 +17,74 @@ cs = digitalio.DigitalInOut(board.D22)
 mcp = MCP.MCP3008(spi, cs)
 
 
-chanz = AnalogIn(mcp, MCP.P0)
-chany = AnalogIn(mcp, MCP.P1)
 chanx = AnalogIn(mcp, MCP.P2)
+chany = AnalogIn(mcp, MCP.P1)
+chanz = AnalogIn(mcp, MCP.P0)
 
-basez = chanz.value
-basey = chany.value
 basex = chanx.value
+basey = chany.value
+basez = chanz.value
 
-lastz = basez
-lasty = basey
 lastx = basex
+lasty = basey
+lastz = basez
 
-tolerance = 750
+tolerance = 1000
 
-print('base (Z: {z}, X: {x}, Y: {y})'.format(z = basez, x = basex, y = basey))
 
-def map(value, from_min, from_max, to_min, to_max):
+def remap(value, from_min, from_max, to_min, to_max):
     # this remaps a value from original range to new range
     left_span = from_max - from_min
     right_span = to_max - to_min
 
     # Convert the left range into a 0-1 range (int)
-    valueScaled = int(value - from_min) / int(left_span)
+    scaled_value = int(value - from_min) / int(left_span)
 
     # Convert the 0-1 range into a value in the right range.
-    return int(to_min + (valueScaled * right_span))
+    return int(to_min + (scaled_value * right_span))
 
 
 first = True
 while True:
     changed = False
 
-    valz = chanz.value
-    valy = chany.value
     valx = chanx.value
+    valy = chany.value
+    valz = chanz.value
 
-    deltaz = abs(valz - lastz)
-    deltay = abs(valy - lasty)
     deltax = abs(valx - lastx)
+    deltay = abs(valy - lasty)
+    deltaz = abs(valz - lastz)
 
-    baseDz = abs(basez - valz)
-    baseDy = abs(basey - valy)
     baseDx = abs(basex - valx)
+    baseDy = abs(basey - valy)
+    baseDz = abs(basez - valz)
 
     # Caculate 360deg values like so: atan2(-yAng, -zAng)
     # atan2 outputs the value of -π to π (radians)
     # We are then converting the radians to degrees
-    mapz = map(valz, 0, 65535, -90, 90)
-    mapy = map(valy, 0, 65535, -90, 90)
-    mapx = map(valx, 0, 65535, -90, 90)
+    mapx = remap(valx, 0, 65535, -90, 90)
+    mapy = remap(valy, 0, 65535, -90, 90)
+    mapz = remap(valz, 0, 65535, -90, 90)
 
-    angz = math.degrees(math.atan2(-mapy, -mapx) + math.pi)
-    angy = math.degrees(math.atan2(-mapx, -mapz) + math.pi)
     angx = math.degrees(math.atan2(-mapy, -mapz) + math.pi)
+    angy = math.degrees(math.atan2(-mapx, -mapz) + math.pi)
+    angz = math.degrees(math.atan2(-mapy, -mapx) + math.pi)
 
     if deltaz > tolerance or deltay > tolerance or deltax > tolerance:
         changed = True
 
     if changed or first:
-        print(' delta (Z: {z}, X: {x}, Y: {y})'.format(z = deltaz, x = deltax, y = deltay))
-        print('actual (Z: {z}, X: {x}, Y: {y})'.format(z = valz, x = valx, y = valy))
-        print(' baseD (Z: {z}, X: {x}, Y: {y})'.format(z = baseDz, x = baseDx, y = baseDy))
-        print('mapped (Z: {z}, X: {x}, Y: {y})'.format(z = mapz, x = mapy, y = mapx))
-        print(' angle (Z: {z}, X: {x}, Y: {y})'.format(z = angz, x = angy, y = angx))
+        print('delta (X: {x}, Y: {y}, Z: {z})'.format(x=deltax, y=deltay, z=deltaz))
+        print('actual (X: {x}, Y: {y}, Z: {z})'.format(x=valx, y=valy, z=valz))
+        print('baseD (X: {x}, Y: {y}, Z: {z})'.format(x=baseDx, y=baseDy, z=baseDz))
+        print('mapped (X: {x}, Y: {y}, Z: {z})'.format(x=mapx, y=mapy, z=mapz))
+        print('angle (X: {x}, Y: {y}, Z: {z})'.format(x=angx, y=angy, z=angz))
         print('')
 
-        lastz = valz
-        lasty = valy
         lastx = valx
+        lasty = valy
+        lastz = valz
 
     first = False
 
